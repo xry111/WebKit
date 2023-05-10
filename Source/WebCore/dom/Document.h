4a04dc572b4f6da32e77f036b0d161b94ec22280
Chris Dumez <cdumez@apple.com>
2023-05-10 14:50:50 -0700

    Cherry-pick 259548.752@safari-7615-branch (a0fa94d1a572). https://bugs.webkit.org/show_bug.cgi?id=256549
        Restrict further top-frame navigations by a third-party iframe
        https://bugs.webkit.org/show_bug.cgi?id=256549
        rdar://108794051
    
        Reviewed by Geoffrey Garen.
    
        Restrict further top-frame navigations by a third-party iframe:
        - Block navigations to a different scheme
        - Block navigations that start off same-site but redirect to a different site
    
        * Source/WebCore/dom/Document.cpp:
        (WebCore::Document::isNavigationBlockedByThirdPartyIFrameRedirectBlocking):
        * Source/WebCore/dom/Document.h:
        * Source/WebCore/loader/DocumentLoader.cpp:
        (WebCore::DocumentLoader::willSendRequest):
        * Source/WebCore/loader/NavigationRequester.cpp:
        (WebCore::NavigationRequester::from):
        * Source/WebCore/loader/NavigationRequester.h:
        (WebCore::NavigationRequester::encode const):
        (WebCore::NavigationRequester::decode):
    
        Canonical link: https://commits.webkit.org/259548.752@safari-7615-branch
    
    (cherry picked from commit 13db18fbb54ae73f4399a2c4addfac94f7bd7e95)

--- Source/WebCore/dom/Document.h
+++ Source/WebCore/dom/Document.h
@@ -1777,6 +1777,8 @@ public:
 
     virtual void didChangeViewSize() { }
 
+    bool isNavigationBlockedByThirdPartyIFrameRedirectBlocking(LocalFrame& targetFrame, const URL& destinationURL);
+
 protected:
     enum class ConstructionFlag : uint8_t {
         Synthesized = 1 << 0,
@@ -1880,7 +1882,6 @@ private:
     void didLoadResourceSynchronously(const URL&) final;
 
     bool canNavigateInternal(LocalFrame& targetFrame);
-    bool isNavigationBlockedByThirdPartyIFrameRedirectBlocking(LocalFrame& targetFrame, const URL& destinationURL);
 
 #if USE(QUICK_LOOK)
     bool shouldEnforceQuickLookSandbox() const;
